cd430cb2fabdd0ca52f104c3590538b2113b0466
Sope57 <jorgecq57@hotmail.com>
2017-01-12 14:38:44 -0800

    Version 1 test

---
+++ src/js/components/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import TextAnimation from './TextAnimation/TextAnimation'
+
+export default class Layout extends React.Component {
+	render() {
+		return (
+			<div>
+				<TextAnimation />
+			</div>
+		);
+	}
+}
---
+++ src/js/components/TextAnimation/TextAnimation.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class TextAnimation extends React.Component {
+	render() {
+		return (
+			<h1>This is a text animation!</h1>
+		);
+	}
+}
