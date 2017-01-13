786acaf0fc5952e144879f2f14550fed69f82dd5
Sope57 <jorgecq57@hotmail.com>
2017-01-13 13:28:04 -0800

    Text animation completed

--- src/js/components/TextAnimation/TextAnimation.js
+++ src/js/components/TextAnimation/TextAnimation.js
@@ -6,41 +6,45 @@ export default class TextAnimation extends React.Component {
 		this.state = {
 			text: "Festival Internacional de Series Web",
 			currentText: "",
-			count: 1
+			count: 0
 		}
 		this.updateText;
 	}
 
-	_updateText() {
+	_updateText(mode) {
 		const { text, currentText, count } = this.state;
-		
-		if (count <= text.length) {
-			this.setState({
-				currentText: text.substr(0, count),
-				count: count + 1
-			});
-		} else {
-			clearInterval(this.updateText);
-			setInterval(()=>{
-				console.log(text);
-				console.log(currentText);
+
+		switch(mode) {
+			case 'write':
+				this.setState({
+					currentText: text.substr(0, count + 1),
+					count: count + 1
+				});
+				break;
+			case 'loop':
 				if (currentText.length == text.length) {
 					this.setState({
-						currentText: text + "__"
+						currentText: text + "_"
 					});
 				} else {
 					this.setState({
 						currentText: text
 					});
 				}
-			}, 1000);
+				break;
 		}
 	}
 
 	componentWillMount() {
 		this.updateText = setInterval(()=>{
-			this._updateText();
-		}, 40);
+			this._updateText("write");
+			if (this.state.count == this.state.text.length) {
+				clearInterval(this.updateText);
+				this.updateText = setInterval(()=>{
+					this._updateText("loop");
+				}, 350);
+			}
+		}, 60);
 	}
 
 	render() {
