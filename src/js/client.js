0595ccd36711f63213a107ab4209b923e2d8e25a
SrPanquesito <luisgdiazreynoso@gmail.com>
2017-01-13 16:52:01 -0800

    nyan

--- src/js/client.js
+++ src/js/client.js
@@ -7,4 +7,14 @@ require('../sass/styles.sass');
 
 const app = document.getElementById('app');
 
+/*function resize(){
+    if ($(window).width() < 1200) { 
+      document.getElementById("main").style.backgroundImage="url(images/bg-mobil.jpg)";
+  } else {
+    document.getElementById("main").style.backgroundImage="url(images/bg-desktop.jpg)";
+  }
+}
+resize();
+$(window).on('resize', resize);*/
+
 ReactDOM.render(<Layout/>, app);
